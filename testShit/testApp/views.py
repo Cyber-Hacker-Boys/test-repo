376e2fdfe44b2b1e70509293e8f922438adb952c
Fabian Nunes <fabiannunes12@gmail.com>
2021-11-05 14:20:37 +0000

    Inheritance shit

--- testShit/testApp/views.py
+++ testShit/testApp/views.py
@@ -6,10 +6,21 @@ from django.http import HttpResponse
 
 def index(request):
     randoms = [
-        {'title': 'Bitch'},
-        {'title': 'Second Bitch'}
+        {
+            'title': 'Bitch',
+            'location': 'Your Mama',
+            'slug': 'Get-Fucket'},
+        {
+            'title': 'Bitch2',
+            'location': 'Your Mama 2',
+            'slug': 'Get-Fucket-2'}
     ]
     return render(request, 'testApp/index.html', {
         'showM': True,
         'randoms': randoms
     })
+
+
+def details(request, shit_slug):
+    selectedshit = {'title': 'This is shit', 'description': 'No Shit'}
+    return render(request, 'testApp/details.html', {'selected': selectedshit})
