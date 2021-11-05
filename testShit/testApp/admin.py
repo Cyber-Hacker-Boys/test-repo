f5d6a5ad9488dcb9214d9ad6b3969c995ea6f77c
Fabian Nunes <fabiannunes12@gmail.com>
2021-11-05 21:34:52 +0000

    Almost done

--- testShit/testApp/admin.py
+++ testShit/testApp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import Jojo
 # Register your models here.
+
+admin.site.register(Jojo)
--- testShit/testApp/views.py
+++ testShit/testApp/views.py
@@ -1,26 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Jojo
+
 
 # Create your views here.
 
 def index(request):
-    randoms = [
-        {
-            'title': 'Bitch',
-            'location': 'Your Mama',
-            'slug': 'Get-Fucket'},
-        {
-            'title': 'Bitch2',
-            'location': 'Your Mama 2',
-            'slug': 'Get-Fucket-2'}
-    ]
+    randoms = Jojo.objects.all()
     return render(request, 'testApp/index.html', {
-        'showM': True,
         'randoms': randoms
     })
 
 
 def details(request, shit_slug):
-    selectedshit = {'title': 'This is shit', 'description': 'No Shit'}
-    return render(request, 'testApp/details.html', {'selected': selectedshit})
+    try:
+        selectedshit = Jojo.objects.get(slug=shit_slug)
+        return render(request, 'testApp/details.html', {'selected': selectedshit, 'meetE': True})
+
+    except Exception as exc:
+        return render(request, 'testApp/details.html', {'meetE': False})
